caadbdc32b65b3eb65f663d9b72c7c8c60a40467
Bence Imre Kiss <kissbenceimre99@gmail.com>
2022-05-26 17:23:13 +0200

    fix after fuckup

--- TopUp1/TopUp1/DAL/StaffDAO.cs
+++ TopUp1/TopUp1/DAL/StaffDAO.cs
@@ -81,10 +81,6 @@ namespace TopUp1.DAL
                     staff.HomeAddress = (string)rdr["HomeAddress"];
                     staff.BirthDate = ((DateTime)rdr["DoB"]).ToShortDateString();
                     staff.Role.RoleName = (string)rdr["RoleName"];
-<<<<<<< HEAD
-                   
-=======
->>>>>>> TestButBetter
                 }
                 rdr.Close();
 
