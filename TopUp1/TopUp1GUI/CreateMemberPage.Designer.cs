711c7642d3c853c5f9103e2f1a3cfcf552af2079
cosminns <cosmin.ns60@yahoo.com>
2022-05-28 12:42:58 +0200

    Create Member

--- TopUp1/TopUp1GUI/CreateMemberPage.Designer.cs
+++ TopUp1/TopUp1GUI/CreateMemberPage.Designer.cs
@@ -29,142 +29,97 @@
         private void InitializeComponent()
         {
             this.labelSSN = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSSN = new System.Windows.Forms.TextBox();
+            this.buttonCreateMember = new System.Windows.Forms.Button();
+            this.textBoxCampusAddress = new System.Windows.Forms.TextBox();
+            this.labelCampusAddress = new System.Windows.Forms.Label();
+            this.textBoxJoinDate = new System.Windows.Forms.TextBox();
+            this.labelJoinDate = new System.Windows.Forms.Label();
+            this.checkBoxIsProfessor = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // labelSSN
             // 
             this.labelSSN.AutoSize = true;
-            this.labelSSN.Location = new System.Drawing.Point(41, 49);
+            this.labelSSN.Location = new System.Drawing.Point(92, 51);
             this.labelSSN.Name = "labelSSN";
-            this.labelSSN.Size = new System.Drawing.Size(38, 15);
+            this.labelSSN.Size = new System.Drawing.Size(28, 15);
             this.labelSSN.TabIndex = 0;
-            this.labelSSN.Text = "label1";
+            this.labelSSN.Text = "SSN";
             // 
-            // textBox1
+            // textBoxSSN
             // 
-            this.textBox1.Location = new System.Drawing.Point(97, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 1;
+            this.textBoxSSN.Location = new System.Drawing.Point(148, 48);
+            this.textBoxSSN.Name = "textBoxSSN";
+            this.textBoxSSN.Size = new System.Drawing.Size(100, 23);
+            this.textBoxSSN.TabIndex = 1;
             // 
-            // button1
+            // buttonCreateMember
             // 
-            this.button1.Location = new System.Drawing.Point(644, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonCreateMember.Location = new System.Drawing.Point(148, 179);
+            this.buttonCreateMember.Name = "buttonCreateMember";
+            this.buttonCreateMember.Size = new System.Drawing.Size(100, 23);
+            this.buttonCreateMember.TabIndex = 2;
+            this.buttonCreateMember.Text = "Create Member";
+            this.buttonCreateMember.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // textBoxCampusAddress
             // 
-            this.textBox2.Location = new System.Drawing.Point(97, 75);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 4;
+            this.textBoxCampusAddress.Location = new System.Drawing.Point(148, 77);
+            this.textBoxCampusAddress.Name = "textBoxCampusAddress";
+            this.textBoxCampusAddress.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCampusAddress.TabIndex = 4;
             // 
-            // label2
+            // labelCampusAddress
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.labelCampusAddress.AutoSize = true;
+            this.labelCampusAddress.Location = new System.Drawing.Point(46, 80);
+            this.labelCampusAddress.Name = "labelCampusAddress";
+            this.labelCampusAddress.Size = new System.Drawing.Size(96, 15);
+            this.labelCampusAddress.TabIndex = 3;
+            this.labelCampusAddress.Text = "Campus Address";
             // 
-            // textBox3
+            // textBoxJoinDate
             // 
-            this.textBox3.Location = new System.Drawing.Point(97, 104);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
-            this.textBox3.TabIndex = 6;
+            this.textBoxJoinDate.Location = new System.Drawing.Point(148, 106);
+            this.textBoxJoinDate.Name = "textBoxJoinDate";
+            this.textBoxJoinDate.Size = new System.Drawing.Size(100, 23);
+            this.textBoxJoinDate.TabIndex = 6;
             // 
-            // label3
+            // labelJoinDate
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 107);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.labelJoinDate.AutoSize = true;
+            this.labelJoinDate.Location = new System.Drawing.Point(26, 114);
+            this.labelJoinDate.Name = "labelJoinDate";
+            this.labelJoinDate.Size = new System.Drawing.Size(116, 15);
+            this.labelJoinDate.TabIndex = 5;
+            this.labelJoinDate.Text = "Join Date automated";
             // 
-            // textBox4
+            // checkBoxIsProfessor
             // 
-            this.textBox4.Location = new System.Drawing.Point(97, 133);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 23);
-            this.textBox4.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(41, 136);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(97, 162);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 23);
-            this.textBox5.TabIndex = 10;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(41, 165);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 15);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "label5";
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(97, 191);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 23);
-            this.textBox6.TabIndex = 12;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(41, 194);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 15);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "label6";
+            this.checkBoxIsProfessor.AutoSize = true;
+            this.checkBoxIsProfessor.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxIsProfessor.Location = new System.Drawing.Point(72, 134);
+            this.checkBoxIsProfessor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBoxIsProfessor.Name = "checkBoxIsProfessor";
+            this.checkBoxIsProfessor.Size = new System.Drawing.Size(86, 19);
+            this.checkBoxIsProfessor.TabIndex = 18;
+            this.checkBoxIsProfessor.Text = "Is Professor";
+            this.checkBoxIsProfessor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxIsProfessor.UseVisualStyleBackColor = true;
             // 
             // CreateMemberPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBoxIsProfessor);
+            this.Controls.Add(this.textBoxJoinDate);
+            this.Controls.Add(this.labelJoinDate);
+            this.Controls.Add(this.textBoxCampusAddress);
+            this.Controls.Add(this.labelCampusAddress);
+            this.Controls.Add(this.buttonCreateMember);
+            this.Controls.Add(this.textBoxSSN);
             this.Controls.Add(this.labelSSN);
             this.Name = "CreateMemberPage";
             this.Text = "CreateMemberPage";
@@ -177,17 +132,12 @@
         #endregion
 
         private Label labelSSN;
-        private TextBox textBox1;
-        private Button button1;
-        private TextBox textBox2;
-        private Label label2;
-        private TextBox textBox3;
-        private Label label3;
-        private TextBox textBox4;
-        private Label label4;
-        private TextBox textBox5;
-        private Label label5;
-        private TextBox textBox6;
-        private Label label6;
+        private TextBox textBoxSSN;
+        private Button buttonCreateMember;
+        private TextBox textBoxCampusAddress;
+        private Label labelCampusAddress;
+        private TextBox textBoxJoinDate;
+        private Label labelJoinDate;
+        private CheckBox checkBoxIsProfessor;
     }
 }
