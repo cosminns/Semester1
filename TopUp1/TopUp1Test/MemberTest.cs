39b36856c7ac4d52c9afda24f284308b2ab2cc84
Bence Imre Kiss <kissbenceimre99@gmail.com>
2022-06-12 23:10:33 +0200

    1 more test

--- TopUp1/TopUp1Test/MemberTest.cs
+++ TopUp1/TopUp1Test/MemberTest.cs
@@ -11,21 +11,47 @@ namespace TopUp1Test
     [TestFixture]
     public class MemberTest
     {
-        Mock<IDAO<Member>> mockMemberDAO = default!;
-        MemberFacade systemUnderTest = default!;
+        IDAO<Member> memberDAO;
+        Member existingMember;
+
+        [SetUp]
+        public void Setup()
+        {
+            SetUpMember();
+            Mock<IDAO<Member>> meberDAOMock = new Mock<IDAO<Member>>(MockBehavior.Loose);
+            meberDAOMock.Setup<Member>
+            (
+                b => b.GetById
+                (
+                    It.Is<Member>(m => m.SSN == existingMember.SSN)
+                )
+            )
+            .Returns(existingMember);
+            memberDAO = meberDAOMock.Object;
+        }
+
+        private void SetUpMember()
+        {
+            existingMember = new Member();
+            existingMember.SSN = 10002354;
+            existingMember.Name = "Monte N Bass";
+        }
 
         [Test]
         [TestCase(10002354, "Monte N Bass")]
-        public void GetMemberTest(int ssn, string expectedName)
+        public void GetMemberHappyPathTest(int ssn, string expectedName)
         {
-            Member member = new Member();
-            mockMemberDAO = new Mock<IDAO<Member>>(MockBehavior.Strict);
-            mockMemberDAO
-                .Setup(x => x.GetById(It.Is<Member>(m => m.SSN == ssn)))
-                .Returns(new Member { Name = expectedName });
-            systemUnderTest = new MemberFacade(mockMemberDAO.Object);
-            member = systemUnderTest.GetMemberInfo(ssn);
+            var member = memberDAO.GetById(new Member { SSN = ssn });
+            Assert.That(member, Is.Not.Null);
             Assert.That(member.Name, Is.EqualTo(expectedName));
+        }
+
+        [Test]
+        [TestCase(123, "123")]
+        public void GetMemberNonexistentPathTest(int ssn, string expectedName)
+        {
+            var member = memberDAO.GetById(new Member { SSN = ssn });
+            Assert.That(member, Is.Null);
         }
     }
 }
